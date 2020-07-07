840b726caec0787f09ae412243449344fcbb82a9
Kar <krlosbatdev@gmail.com>
2020-07-07 19:45:57 -0400

    Update sign up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { AmplifyAuthenticator } from '@aws-amplify/ui-react';
+import { AmplifyAuthenticator, AmplifySignIn, AmplifySignUp } from '@aws-amplify/ui-react';
 import './App.css';
 import Amplify from "aws-amplify";
 import awsExports from "./aws-exports";
@@ -11,8 +11,28 @@ const Home = lazy(() => import('./components/Home'));
 Amplify.configure(awsExports);
 
 const App = () => {
+  const signUpFields = [
+    {
+      type: "email",
+      label: "Email address",
+      required: true,
+    },
+    {
+      type: "password",
+      label: "Password",
+      required: true,
+    }
+  ];
+
   return (
-    <AmplifyAuthenticator>
+    <AmplifyAuthenticator usernameAlias="email">
+      <AmplifySignUp
+        headerText="Sign Up"
+        usernameAlias="email"
+        slot="sign-up"
+        formFields={signUpFields}
+      />
+      <AmplifySignIn slot="sign-in" usernameAlias="email" />
       <Router>
         <Suspense fallback={<div>Loading...</div>}>
           <Navbar />
