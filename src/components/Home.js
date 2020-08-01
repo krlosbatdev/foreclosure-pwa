6edd9bc799c1e914565babe600adda273f671e01
Kar <krlosbatdev@gmail.com>
2020-08-01 04:21:20 -0400

    order by date

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 import { listScraps } from '../graphql/queries';
@@ -11,49 +11,49 @@ function Home(props) {
 
   const getScraps = async () => {
 
-    const scraps = await API.graphql(graphqlOperation(listScraps));
+    const scraps = await API.graphql(graphqlOperation(listScraps, { sortDirection: "ASC" }));
 
-    let scrapList = scraps.data.listScraps["items"].map(i => {
-      return {
-        date: i.id.substr(0, i.id.indexOf(',')),
-        miamidade: i.md_forclosures,
-        florida: i.fl_forclosures
-
-      }
-    });
+    let scrapList = scraps.data.listScraps["items"]
+      .map(i => {
+        return {
+          date: new Date(i.id),
+          miamidade: i.md_forclosures,
+          florida: i.fl_forclosures
+        }
+      }).sort((a, b) => a.date - b.date);
     setItems(scrapList)
   }
-  useEffect(() =>  {
+  useEffect(() => {
     getScraps();
   }, [])
 
 
-    return (
-      <div>
+  return (
+    <div>
       {items.length ?
 
-      <LineChart
-        width={350}
-        height={300}
-        data={items}
-        margin={{
-          top: 30, right: 30, left: 20, bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="florida" stroke="#8884d8" activeDot={{ r: 8 }} />
-        <Line type="monotone" dataKey="miamidade" stroke="#82ca9d" />
-      </LineChart>
+        <LineChart
+          width={350}
+          height={300}
+          data={items}
+          margin={{
+            top: 30, right: 30, left: 20, bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="florida" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="miamidade" stroke="#82ca9d" />
+        </LineChart>
 
 
-      : <span>Loading ...</span>
-    }
-      </div>
-    )
+        : <span>Loading ...</span>
+      }
+    </div>
+  )
 }
 
 export default Home;
